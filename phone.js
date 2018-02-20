7c163468cd233a9d2d7c02a6342aafdcb0e0637d
jbp173 <jordanpeters@comcast.net>
2018-02-20 00:20:39 -0700

    Init

---
+++ phone.js
@@ -0,0 +1,8 @@
+$(document).ready(function() { //do this when the document is loaded
+	$("#alert").hide(); 
+});
+
+$("#submit").click(function() { //when "button_id is clicked"
+	$("#alert").show();
+	
+});
